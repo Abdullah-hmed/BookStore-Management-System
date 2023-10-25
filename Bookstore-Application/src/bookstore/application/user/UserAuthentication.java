ee5cd31563e75e135b617cb286fcfa8a9dba5ec3
LaibaAsif126 <laibaasif344@gmail.com>
2023-10-25 13:21:05 +0100

    Added new user authentication class

---
+++ Bookstore-Application/src/bookstore/application/user/UserAuthentication.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.application.user;
+
+/**
+ *
+ * @author Laiba Asif
+ */
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserAuthentication {
+    private Map<String, User> users;
+    
+    public UserAuthentication() {
+        users = new HashMap<>();
+    }
+    
+    public boolean register(User user) {
+        if (users.containsKey(user.getUsername())) {
+            return false; // User already exists, registration failed
+        }
+        
+        String hashedPassword = hashPassword(user.getPassword());
+        user.setPassword(hashedPassword);
+        
+        users.put(user.getUsername(), user);
+        return true; // Registration successful
+    }
+    
+    public boolean login(String username, String password) {
+        if (!users.containsKey(username)) {
+            return false; // User does not exist, login failed
+        }
+        
+        User user = users.get(username);
+        String hashedPassword = hashPassword(password);
+        
+        return user.getPassword().equals(hashedPassword);
+    }
+    
+    private String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b));
+            }
+            
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+}
