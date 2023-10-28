7f208848f849ddf092b4ae6fb36fcc0a06b2068a
LaibaAsif126 <laibaasif344@gmail.com>
2023-10-28 11:48:31 +0100

    new commit

--- Bookstore-Application/src/Cart/Cart.java
+++ Bookstore-Application/src/Cart/Cart.java
@@ -9,6 +9,34 @@ package Cart;
  *
  * @author Laiba Asif
  */
+
+
+import bookstore.application.Model.Book;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
-    
+    private List<Book> items;
+
+    public Cart() {
+        items = new ArrayList<>();
+    }
+
+    public void addToCart(Book book) {
+        items.add(book);
+    }
+
+    public void removeFromCart(Book book) {
+        items.remove(book);
+    }
+
+    public List<Book> getItems() {
+        return items;
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+
+
 }
