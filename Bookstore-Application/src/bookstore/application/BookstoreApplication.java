a847c49f529b1ec99f9e242e2cb903c840bddc56
Abdullah-hmed <abdullahwyi@gmail.com>
2023-10-31 23:18:14 -0700

    Dynamic Window Size

--- Bookstore-Application/src/bookstore/application/BookstoreApplication.java
+++ Bookstore-Application/src/bookstore/application/BookstoreApplication.java
@@ -7,8 +7,10 @@ package bookstore.application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -16,14 +18,21 @@ import javafx.stage.Stage;
  * @author SC
  */
 public class BookstoreApplication extends Application {
-    
+    Screen screen = Screen.getPrimary();
+    Rectangle2D bounds = screen.getVisualBounds();
+    double screenWidth = bounds.getWidth();
+    double screenHeight = bounds.getHeight();
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        stage.setMinHeight(890);
-        stage.setMinWidth(1315);
+        //stage.setMinHeight(890);
+        //stage.setMinWidth(1315);
+        stage.setWidth(screenWidth * 0.8);  // Adjust these values as needed
+        stage.setHeight(screenHeight * 0.8);
+
         stage.setScene(scene);
         stage.show();
     }
