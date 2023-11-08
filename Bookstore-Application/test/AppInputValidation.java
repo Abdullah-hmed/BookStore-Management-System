8fd24843b84a4aa810be52dd4596fe0d9748c844
Abdullah-hmed <abdullahwyi@gmail.com>
2023-11-08 18:28:23 +0500

    Unit Tests Implemented

---
+++ Bookstore-Application/test/AppInputValidation.java
@@ -0,0 +1,57 @@
+import bookstore.application.Model.Book;
+import bookstore.application.Model.Database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author stu
+ */
+public class AppInputValidation {
+    
+    public AppInputValidation() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+        
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void LoginValidation() {
+        String username = "laiba";
+        String password = "laiba";
+        //result values generated here
+        Database database = new Database();
+        boolean result = database.loginUser(username, password);
+        
+        assertEquals(true, result);
+    }
+}
